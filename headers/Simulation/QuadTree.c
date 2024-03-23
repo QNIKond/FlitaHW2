3b0f200478e52b77a2ae1025c26eab5d5dd21009
NIKond <ndkokurin@yandex.ru>
2024-03-23 11:37:31 +0300

    Optimized quadtree

--- headers/Simulation/QuadTree.c
+++ headers/Simulation/QuadTree.c
@@ -14,7 +14,35 @@ void PlaceInList(Graph* graph,qtID list, nodeID node){
     GETQTNODES(graph)[list].children[0] = id;
 }
 
-void PlaceNode(Graph* graph,qtID root, nodeID node,Rectangle bounds, int maxDepth)
+void PlaceNode(Graph* graph,qtID root, nodeID node,Rectangle center, int maxDepth)
+{
+    qtID curNode = root;
+    Rectangle curCenter = center;
+    int child = GetChild(GETNODES(graph)[node].pos,curCenter);
+    int depth = 0;
+    while(GETQTNODES(graph)[curNode].children[child] != -1){
+        curNode = GETQTNODES(graph)[curNode].children[child];
+        curCenter = (Rectangle){curCenter.x+curCenter.width*(float)((child%2)*2-1)
+                ,curCenter.y+curCenter.height*(float)((child/2)*2 -1),
+                                curCenter.width/2,curCenter.height/2};
+        child = GetChild(GETNODES(graph)[node].pos,curCenter);
+        ++depth;
+        if(depth >= maxDepth){
+            PlaceInList(graph, curNode,node);
+            return;
+        }
+    }
+    int id = -1;
+    *(QuadTree*) Alloc(&graph->qtree,&id) = (QuadTree){node,{},0,{-1,-1,-1,-1},1};
+    GETQTNODES(graph)[curNode].children[child] = id;
+    if(GETQTNODES(graph)[curNode].isLeaf){
+        GETQTNODES(graph)[curNode].isLeaf = 0;
+        //PlaceNode(graph,root,GETQTNODES(graph)[curNode].node,bounds,maxDepth);
+        PlaceNode(graph,curNode,GETQTNODES(graph)[curNode].node,curCenter,maxDepth-depth);
+    }
+}
+
+/*void PlaceNode(Graph* graph,qtID root, nodeID node,Rectangle bounds, int maxDepth)
 {
     qtID curNode = root;
     Rectangle curCenter = {bounds.width/2,bounds.height/2,bounds.width/4,bounds.height/4};
@@ -31,15 +59,21 @@ void PlaceNode(Graph* graph,qtID root, nodeID node,Rectangle bounds, int maxDept
             PlaceInList(graph, curNode,node);
             return;
         }
+        if(GETQTNODES(graph)[curNode].children[child] == -1){
+            int id = -1;
+            *(QuadTree*) Alloc(&graph->qtree,&id) = (QuadTree){node,{},0,{-1,-1,-1,-1},1};
+            GETQTNODES(graph)[curNode].children[child] = id;
+            if(GETQTNODES(graph)[curNode].isLeaf){
+                GETQTNODES(graph)[curNode].isLeaf = 0;
+                node = GETQTNODES(graph)[curNode].node;
+                //PlaceNode(graph,root,GETQTNODES(graph)[curNode].node,bounds,maxDepth);
+            }
+            else{
+                break;
+            }
+        }
     }
-    int id = -1;
-    *(QuadTree*) Alloc(&graph->qtree,&id) = (QuadTree){node,{},0,{-1,-1,-1,-1},1};
-    GETQTNODES(graph)[curNode].children[child] = id;
-    if(GETQTNODES(graph)[curNode].isLeaf){
-        GETQTNODES(graph)[curNode].isLeaf = 0;
-        PlaceNode(graph,root,GETQTNODES(graph)[curNode].node,bounds,maxDepth);
-    }
-}
+}*/
 
 
 
@@ -49,5 +83,5 @@ void BuildQuadTree(Graph *graph,Rectangle bounds, int maxDepth)
     *(QuadTree *)Alloc(&graph->qtree,0) =
             (QuadTree){0,(Vector2){0,0},0,{-1,-1,-1,-1},1};
     for(int i = 1; i < graph->nodes.filled; ++i)
-        PlaceNode(graph,0,i,bounds,maxDepth);
+        PlaceNode(graph,0,i,(Rectangle){bounds.width/2,bounds.height/2,bounds.width/4,bounds.height/4},maxDepth);
 }
